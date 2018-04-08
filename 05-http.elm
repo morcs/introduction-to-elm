cc877fd645ba52381f8931f21724fb270c36b3b9
James Morcom <morcs@morcs.com>
2018-04-08 15:40:06 +0100

    Update 05-http.elm

--- 05-http.elm
+++ 05-http.elm
@@ -11,12 +11,14 @@ import Json.Decode.Pipeline
 main =
     Html.program { init = init, update = update, view = view, subscriptions = always Sub.none }
 
+
 init =
-  (Loading, Http.send Loaded (Http.get "https://l9axnk5c93.execute-api.us-east-1.amazonaws.com/dev" decodeResponse))
+    ( Loading, Http.send Loaded (Http.get "https://l9axnk5c93.execute-api.us-east-1.amazonaws.com/dev" decodeResponse) )
 
 
 decodeResponse =
-  Json.Decode.list decodeCard
+    Json.Decode.list decodeCard
+
 
 type alias Card =
     { imgUrl : String, name : String, nonchalance : Int, aggression : Int, glamour : Int, speed : Int }
@@ -27,29 +29,32 @@ type Model
     | Ok (List Card) (Maybe Card)
     | Error String
 
+
 type Msg
     = Select Card
     | Loaded (Result Http.Error (List Card))
 
 
 update msg model =
-    case (model, msg) of
+    case msg of
+        Select card ->
+            case model of
+                Ok cards _ ->
+                    ( Ok cards (Just card), Cmd.none )
 
+                _ ->
+                    ( model, Cmd.none )
 
-        (Ok cards _, Select card) ->
-            (Ok cards (Just card), Cmd.none)
-        (_, Loaded (Result.Ok cards)) ->
-            (Ok cards Nothing, Cmd.none)
-        (_, Loaded (Result.Err err)) ->
-            (Error (toString err), Cmd.none)
-        _ ->
-            (model, Cmd.none)
+        Loaded (Result.Ok cards) ->
+            ( Ok cards Nothing, Cmd.none )
 
+        Loaded (Result.Err err) ->
+            case err of
+                BadStatus { body } ->
+                    ( Error body, Cmd.none )
 
-
---main = view (Ok gifUrls)
---main = view (Error "Could not load images")
---main = view Loading
+                _ ->
+                    ( Error "Unknown error", Cmd.none )
 
 
 renderStat label value =
@@ -60,62 +65,62 @@ renderStat label value =
 
 
 renderCard card =
-    div [ class "col-sm-6" ]
-        [ div [ class "card my-2" ]
-            [ div [ class "card-header" ]
-                [ text card.name ]
-            , div [ class "my-0 font-weight-normal" ]
-                [ img [ class "img-fluid", src card.imgUrl ] []
-                , table [ class "table mb-0" ]
-                    [ renderStat "Nonchalance" card.nonchalance
-                    , renderStat "Aggression" card.aggression
-                    , renderStat "Glamour" card.glamour
-                    , renderStat "Speed" card.speed
-                    ]
-                , button [ onClick (Select card) ] [ text "Select" ]
+    div [ class "card my-2", onClick (Select card) ]
+        [ div [ class "card-header" ]
+            [ text card.name ]
+        , div [ class "my-0 font-weight-normal" ]
+            [ img [ class "img-fluid", src card.imgUrl ] []
+            , table [ class "table mb-0" ]
+                [ renderStat "Nonchalance" card.nonchalance
+                , renderStat "Aggression" card.aggression
+                , renderStat "Glamour" card.glamour
+                , renderStat "Speed" card.speed
                 ]
             ]
         ]
 
 
-renderSelectedCard selected =
-    div [ class "jumbotron" ]
-        (case selected of
-            Nothing ->
-                []
-
-            Just card ->
-                [ renderCard card ]
-        )
+renderTopBar selected =
+    case selected of
+        Nothing ->
+            div [ class "fixed-top bg-success text-white p-3" ]
+                [ text "Please select a card" ]
+        Just card ->
+            div [ class "fixed-top bg-primary text-white p-3" ]
+                [ text "Selected: "
+                , strong [] [ text card.name ]
+                ]
 
 
 view model =
-    div [ class "container" ]
-        [ case model of
-            Loading ->
-                div []
-                    [ text "Loading..."
-                    , img [ src "https://morcs.com/introduction-to-elm/loading.gif" ] []
-                    ]
+    case model of
+        Loading ->
+            div [ class "alert alert-success" ]
+                [ text "Loading..." ]
 
-            Ok cards selected ->
-                div []
-                    [ renderSelectedCard selected
-                    , div
-                        [ class "row" ]
-                        (List.map renderCard cards)
-                    ]
+        Ok cards selected ->
+          div []
+           [
+            renderTopBar selected
+           ,main_ [ attribute "role" "main", class "container my-5 py-2" ]
+                  [ 
+                   div
+                     [ class "row" ]
+                     (List.map (\c -> div [ class "col-sm-6 clickable" ] [ renderCard c ]) cards)
+                  ]
+           ]
 
-            Error msg ->
-                div [] [ text ("Error: " ++ msg) ]
-        ]
+        Error msg ->
+            div [ class "alert alert-danger" ]
+                [ text ("Error: " ++ msg) ]
+
 
 decodeCard : Json.Decode.Decoder Card
 decodeCard =
     Json.Decode.Pipeline.decode Card
-        |> Json.Decode.Pipeline.required "imgUrl" (Json.Decode.string)
-        |> Json.Decode.Pipeline.required "name" (Json.Decode.string)
-        |> Json.Decode.Pipeline.required "nonchalance" (Json.Decode.int)
-        |> Json.Decode.Pipeline.required "aggression" (Json.Decode.int)
-        |> Json.Decode.Pipeline.required "glamour" (Json.Decode.int)
-        |> Json.Decode.Pipeline.required "speed" (Json.Decode.int)
+        |> Json.Decode.Pipeline.required "imgUrl" Json.Decode.string
+        |> Json.Decode.Pipeline.required "name" Json.Decode.string
+        |> Json.Decode.Pipeline.required "nonchalance" Json.Decode.int
+        |> Json.Decode.Pipeline.required "aggression" Json.Decode.int
+        |> Json.Decode.Pipeline.required "glamour" Json.Decode.int
+        |> Json.Decode.Pipeline.required "speed" Json.Decode.int
