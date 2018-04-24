312fa8f9f62a106bd5584b4c12951600c227e206
James Morcom <morcs@morcs.com>
2018-04-24 16:19:03 +0100

    Update 02-states-done.elm

--- 02-states-done.elm
+++ 02-states-done.elm
@@ -3,47 +3,96 @@ module Main exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-type Model 
-  = Loading
-  | Ok (List String)
-  | Error String
 
-gifUrls =
-    [ "https://media.giphy.com/media/qoxM1gi6i0V9e/giphy.gif"
-    , "https://media.giphy.com/media/CEdVDTtkvSelO/giphy.gif"
-    , "https://media.giphy.com/media/TKfywHrPHpJiE/giphy.gif"
-    , "https://media.giphy.com/media/cqG5aFdTkk5ig/giphy.gif"
-    , "https://media.giphy.com/media/QsTKxTfou4SSk/giphy.gif"
-    , "https://media.giphy.com/media/BkhOTDvASCfwk/giphy.gif"
-    , "https://media.giphy.com/media/mjvwvf7Udt8rK/giphy.gif"
-    , "https://media.giphy.com/media/1UnVU7zrZr3cQ/giphy.gif"
-    , "https://media.giphy.com/media/I04JymgGbnlgk/giphy.gif"
-    , "https://media.giphy.com/media/VM1JL42ALn0UU/giphy.gif"
-    , "https://media.giphy.com/media/RlBtYmW0BGUHS/giphy.gif"
-    , "https://media.giphy.com/media/bMnnmNo087fgs/giphy.gif"
+main : Html msg
+main =
+    view model
+
+
+
+-- MODEL
+
+
+type Model
+    = Loading
+    | CardList (List Card)
+    | Error String
+
+
+type alias Card =
+    { imgUrl : String
+    , name : String
+    , nonchalance : Int
+    , aggression : Int
+    , glamour : Int
+    , speed : Int
+    }
+
+
+testCards : List Card
+testCards =
+    [ { imgUrl = "https://media.giphy.com/media/QsTKxTfou4SSk/giphy.gif", name = "The Joneses", nonchalance = 8, aggression = 6, glamour = 4, speed = 5 }
+    , { imgUrl = "https://media.giphy.com/media/mjvwvf7Udt8rK/giphy.gif", name = "Charlotte", nonchalance = 5, aggression = 9, glamour = 4, speed = 8 }
+    , { imgUrl = "https://media.giphy.com/media/qoxM1gi6i0V9e/giphy.gif", name = "David", nonchalance = 8, aggression = 4, glamour = 8, speed = 10 }
+    , { imgUrl = "https://media.giphy.com/media/CEdVDTtkvSelO/giphy.gif", name = "Mike", nonchalance = 10, aggression = 2, glamour = 9, speed = 7 }
+    , { imgUrl = "https://media.giphy.com/media/TKfywHrPHpJiE/giphy.gif", name = "Helen", nonchalance = 7, aggression = 10, glamour = 6, speed = 4 }
+    , { imgUrl = "https://media.giphy.com/media/BkhOTDvASCfwk/giphy.gif", name = "David & Bill", nonchalance = 9, aggression = 1, glamour = 7, speed = 3 }
     ]
 
-renderCat url =
-    div [ class "cat" ]
-        [ a []
-            [ img [ src url ] []
+
+model : Model
+model =
+    -- Try changing between these in order to test
+    --Loading
+    CardList testCards
+    --Error "An error happened"
+    
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    case model of
+        Loading ->
+            div [ class "alert alert-success" ]
+                [ text "Loading..." ]
+
+        CardList cards ->
+            div []
+                [ main_ [ attribute "role" "main", class "container my-5 py-2" ]
+                    [ div
+                        [ class "row" ]
+                        (List.map renderCard cards)
+                    ]
+                ]
+
+        Error msg ->
+            div [ class "alert alert-danger" ]
+                [ text ("Error: " ++ msg) ]
+
+
+renderCard : Card -> Html msg
+renderCard card =
+    div [ class "col-sm-6 col-md-4" ]
+        [ div [ class "card my-2" ]
+            [ div [ class "card-header" ]
+                [ text card.name ]
+            , div [ class "my-0 font-weight-normal" ]
+                [ img [ class "img-fluid", src card.imgUrl ] []
+                , table [ class "table mb-0" ]
+                    [ renderStat "Nonchalance" card.nonchalance
+                    , renderStat "Aggression" card.aggression
+                    , renderStat "Glamour" card.glamour
+                    , renderStat "Speed" card.speed
+                    ]
+                ]
             ]
         ]
 
-view model =
-  case model of
-    Loading ->
-      div [] 
-          [ text "Loading..." 
-          , img [ src "https://morcs.com/introduction-to-elm/loading.gif" ] []
-          ]
-    Ok images ->
-      div []
-        (List.map renderCat images)
-    Error msg ->
-      div [] [ text ("Error: " ++ msg) ]
 
---main = view (Ok gifUrls)
---main = view (Error "Could not load images")
-main = view Loading
-
+renderStat : String -> Int -> Html msg
+renderStat label value =
+    tr []
+        [ th [] [ text label ]
+        , td [ class "text-right" ] [ text (toString value) ]
+        ]
